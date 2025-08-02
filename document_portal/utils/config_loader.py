1a597ba27bd8fb14cb21590434b41520b65d1e10
HamshikaRam <hamshika.ram@gmail.com>
2025-08-02 11:31:39 +0530

    added config code

---
+++ document_portal/utils/config_loader.py
@@ -0,0 +1,9 @@
+import yaml
+
+def load_config(config_filepath: str = "config/config.yaml") -> dict:
+    with open(config_filepath, 'r') as file:
+        config = yaml.safe_load(file)
+        #print(config)
+        return config
+  
+load_config("config/config.yaml")  
